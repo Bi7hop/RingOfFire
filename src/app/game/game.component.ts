ffcb3d1cb4bfa235ee3c5cf6e81f88fffa208c5c
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-26 10:00:05 +0200

    added shuffle function

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
+import { log } from 'console';
 
 @Component({
   selector: 'app-game',
@@ -10,6 +12,19 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game!: Game;
+
+constructor () {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game= new Game();
+    console.log(this.game);
+    
+  }
 
   takeCard(){
     this.pickCardAnimation = true;
