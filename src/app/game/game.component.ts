067d719d9ee0fc46667e562b3298d5bdca093888
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-26 10:56:07 +0200

    added player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
 import { log } from 'console';
+import { PlayerComponent } from "../player/player.component";
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
