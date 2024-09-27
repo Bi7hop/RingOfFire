d9c0e703911660f09fa4c48c778a648ddea701f4
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-27 09:21:42 +0200

    bugfixes

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule,  } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatIconButton } from '@angular/material/button';
+import { MatDialogRef } from '@angular/material/dialog';
 
 
 
@@ -23,10 +24,17 @@ export class DialogAddPlayerComponent {
 
   name: string = '';
 
+  constructor(public dialogRef: MatDialogRef<DialogAddPlayerComponent>) {
+    
+  }
+
+  ngOnInit(): void {
+
+  }
 
 
   onNoClick(){
-    
+    this.dialogRef.close();
   }
 
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -64,8 +64,9 @@ openDialog(): void {
   const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
   dialogRef.afterClosed().subscribe((name: string) => {
+    if(name && name.length > 0) {
     this.game.players.push(name);
-    
+    }
   });
 }
 
